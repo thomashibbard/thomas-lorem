d22b9e7db09a435884135fdd9d0837e6ff052afc
Thomas Hibbard <thibbard@Thomas-Hibbards.local>
2016-10-19 17:32:01 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var writeFile = require('fs-extra').writeFile
+	,	parser = require('filesize-parser')
+	, argv = require('yargs').argv
+	, join = require('path').join
+	, string = require('./src-str').string;
+
+const directory = process.cwd();
+const stringSize = Buffer.byteLength(string, 'utf8');
+const max = 104857600;
+const min = stringSize;
+var sizeArg = argv.size || argv.s;
+var parsedSizeArg = parser(sizeArg);
+var desiredFileSize = parsedSizeArg >= max ? max :
+											parsedSizeArg <= min ? min : parsedSizeArg;
+
+var repeatAmount = parseInt((desiredFileSize / stringSize), 10);
+var stringToPrint = string.repeat(repeatAmount)
+var fileName = `${sizeArg}_sample_${+new Date()}.txt`;
+var fullPath = join(directory, fileName);
+console.log(fullPath);
+writeFile(directory + '/' + fileName, stringToPrint, 'utf8', (err) => {
+	if(err){
+		console.error(`error creating file ${err}`);
+	}else{
+		console.info(`file written: ${fileName}`)
+	}
+});
